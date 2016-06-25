6e5ab73a9d6c075be1efa1bd4ed58882e1c9d581
kanjiz1 <kanjis007@yahoo.com>
2016-06-25 17:44:21 -0500

    Initial Commit

---
+++ multiples-/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiples-
+//
+//  Created by Oforkanji Odekpe on 6/25/16.
+//  Copyright © 2016 Oforkanji Odekpe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
